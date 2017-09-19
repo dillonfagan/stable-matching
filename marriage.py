738db823063baf50043a51dd5ae3695317e37fec
ryansmith88888 <ryansmith88888@my.uri.edu>
2017-09-19 18:35:32 -0400

    Almost Final Rev
    Comments and Such

--- marriage.py
+++ marriage.py
@@ -5,10 +5,10 @@ from sys import argv
 
 class Person:
     def __init__(self, name):
-        self.name = name
-        self.preferences = []
-        self.next_proposal = 0
-        self.engaged_to = None
+        self.name = name  # initialized invariant
+        self.preferences = []  # initialized invariant
+        self.next_proposal = 0  # initialized invariant
+        self.engaged_to = None  # initialized invariant
 
     def rank_of(self, person):
         return self.preferences.index(person.name)
@@ -25,6 +25,8 @@ def read_people(input_file):
     if not input_file:
         exit(1)
 
+    K = []  # initialized invariant
+    L = []  # initialized invariant
 
     with open(input_file) as f:
         # read the number of matches from first line
@@ -58,23 +60,33 @@ def print_matches(K):
 
 
 def marriage(M, W):
+    # while there exists a knight not engaged and who hasn't preposed to
+    # every lady
     while any(not m.engaged_to and m.next_proposal < len(W) for m in M):
         for m in M:
             if m.engaged_to or m.next_proposal >= len(W):
                 continue
 
             # knight selects his next highest ranked lady to propose to
+            w_name = m.preferences[m.next_proposal]  # initialized invariant
+            w = next(w_ for w_ in W if w_.name == w_name) # initialized invariant
+            m.next_proposal += 1  # maintenance invariant
 
+            # if the given w name does not exist in W, exit
             if not w:
                 error.write("A lady with the name " + w_name + " could not be found.")
                 exit(1)
 
+            # if the lady is not engaged, k and l become engaged
+            # otherwise, if the lady prefers k to her current engagement,
+            # k and l become engaged and k' is free
             if not w.engaged_to:
                 Person.engage(m, w)
             elif w.rank_of(m) < w.rank_of(w.engaged_to):
                 w.engaged_to.disengage()
                 Person.engage(m, w)
 
+    print_matches(M)  # print final matches, termination invariant
 
 
 def main():
